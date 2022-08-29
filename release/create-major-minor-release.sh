21f75ed56d2de5ae108c14204b1ed4d1198f3fe7
Fredrik Liv <fredrik.liv@elastisys.com>
2022-08-29 11:47:17 +0200

    Release fix

--- release/create-major-minor-release.sh
+++ release/create-major-minor-release.sh
@@ -3,7 +3,8 @@
 set -eu -o pipefail
 
 current_branch=$(git symbolic-ref -q --short HEAD)
-if [[ ! "${current_branch}" =~ ^release-[0-9]+\.[0-9]+$ ]]; then
+commit_lookback=10
+if [[ ! "${current_branch}" =~ ^release-[0-9]+-[0-9]+$ ]]; then
   #TODO Log something here
   exit 1
 fi
@@ -15,3 +16,22 @@ minor_version=$(echo "${current_branch}" | sed 's/release-[0-9]\+-\([0-9]\+\)/\1
 
 git tag "v${major_version}.${minor_version}.0"
 git push --tags
+
+if ! git log -1 --format=%s | grep -P "^Reset changelog for release v${major_version}.${minor_version}.0$" > /dev/null; then
+  echo "Changes in QA"
+  # Merge to main
+
+  release_head_commit=$(git rev-parse --short HEAD)
+  reset_commit=$(git log "-${commit_lookback}" --oneline | grep -P "Reset changelog for release v0.1.0$" | awk '{print $1}')
+
+  git switch main
+  git pull
+
+  git switch -c "patches-from-release-${major_version}-${minor_version}"
+
+  for commit in $(git log "${reset_commit}..${release_head_commit}" --format=%h); do
+    git cherry-pick "${commit}"
+  done
+else
+  echo "No changes in QA"
+fi
